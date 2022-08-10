495190d0a7b3545c38ae385ad222ce9da0bb298c
InonBer <inon551@gmail.com>
2022-08-10 16:50:13 +0300

    pulling

--- frontend/src/components/review-list.vue
+++ frontend/src/components/review-list.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="review-list-container">
-        <reviewPrev v-for="review in revies" :review="review" :key="review._id"></reviewPrev>
+        <reviewPrev v-for="review in reviews" :review="review" :key="review._id"></reviewPrev>
     </div>
 </template>
  <script>
 import reviewPrev from './review-prev.vue';
 export default {
+    props: {
+        reviews: Array
+    },
     name: 'reviewList',
     components: {
         reviewPrev
